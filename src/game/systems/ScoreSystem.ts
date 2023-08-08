a566c1d1991eab953f4ee7a591ede1ccd7536230
Dave Kerr <dakerr@outlook.com>
2023-08-07 20:50:43 -0400

    Setting up deployment part3

--- src/game/systems/ScoreSystem.ts
+++ src/game/systems/ScoreSystem.ts
@@ -1,5 +1,5 @@
-import { Game } from "../Game";
-import { System } from "../SystemRunner";
+import { Game } from '../Game';
+import { System } from '../SystemRunner';
 
 export class ScoreSystem implements System {
   /**
